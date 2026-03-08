35c49609fc6880ff791b865fb6346fd9ce896b6d
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-03-08 12:44:40 +0200

    ++

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,15 +1,81 @@
 import { useState } from "react";
+import { useRef } from "react"
+import { useEffect } from "react"
 export const Feedback = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0) 
-    const [bad, setBad] = useState(0) 
+    const [bad, setBad] = useState(0)
+    const btnGood = useRef(null)
+    const btnNeutral = useRef(null)
+    const btnBad = useRef(null)
+    useEffect(()=> {
+        return ()=> {
+            window.addEventListener("keydown", (e)=>{
+                if (e.key === "1") {
+                    btnGood.current.style.backgroundColor = buttonStyle.backgroundColor;
+                    btnGood.current.style.color = buttonStyle.color;
+                    btnGood.current.style.border = buttonStyle.border;
+                    setGood(good + 1);
+                } else if (e.key === "2") {
+                    btnNeutral.current.style.backgroundColor = buttonStyle.backgroundColor;
+                    btnNeutral.current.style.color = buttonStyle.color;
+                    btnNeutral.current.style.border = buttonStyle.border;
+                    setNeutral(neutral + 1);
+                } else if (e.key === "3") {
+                    btnBad.current.style.backgroundColor = buttonStyle.backgroundColor;
+                    btnBad.current.style.color = buttonStyle.color;
+                    btnBad.current.style.border = buttonStyle.border;
+                    setBad(bad + 1);
+                }
+            })
+        }
+    }, [good, neutral, bad]) 
+    const buttonStyle = {
+        backgroundColor:'black',
+        color: 'white',
+        border: '1px solid white',
+      };
+
+      const buttonStyle1 = {
+        backgroundColor:'white',
+        color: 'black',
+        border: '1px solid black',
+        cursor: 'pointer',
+      };
 
     const increment = (type) => {
+        window.addEventListener("keydown", (e)=>{
+            if (e.key === "1") {
+                btnGood.current.style.backgroundColor = buttonStyle.backgroundColor;
+                btnGood.current.style.color = buttonStyle.color;
+                btnGood.current.style.border = buttonStyle.border;
+                setGood(good + 1);
+            } else if (e.key === "2") {
+                btnNeutral.current.style.backgroundColor = buttonStyle.backgroundColor;
+                btnNeutral.current.style.color = buttonStyle.color;
+                btnNeutral.current.style.border = buttonStyle.border;
+                setNeutral(neutral + 1);
+            } else if (e.key === "3") {
+                btnBad.current.style.backgroundColor = buttonStyle.backgroundColor;
+                btnBad.current.style.color = buttonStyle.color;
+                btnBad.current.style.border = buttonStyle.border;
+                setBad(bad + 1);
+            }
+        })
         if (type == "good") {
+            btnGood.current.style.backgroundColor = buttonStyle1.backgroundColor;
+            btnGood.current.style.color = buttonStyle1.color;
+            btnGood.current.style.border = buttonStyle1.border;
             setGood(good + 1);
         } else if (type == "neutral") {
+            btnNeutral.current.style.backgroundColor = buttonStyle1.backgroundColor;
+            btnNeutral.current.style.color = buttonStyle1.color;
+            btnNeutral.current.style.border = buttonStyle1.border;
             setNeutral(neutral + 1);
         } else if (type == "bad") {
+            btnBad.current.style.backgroundColor = buttonStyle1.backgroundColor;
+            btnBad.current.style.color = buttonStyle1.color;
+            btnBad.current.style.border = buttonStyle1.border;
             setBad(bad + 1);
         }
     }
@@ -33,6 +99,7 @@ export const Feedback = () => {
 
             <div>
                 <button
+                    ref={btnGood}
                     onClick={() => increment("good")}
                     type="button"
                 >
@@ -40,6 +107,7 @@ export const Feedback = () => {
                 </button>
 
                 <button
+                    ref={btnNeutral}
                     onClick={() => increment("neutral")}
                     type="button"
                 >
@@ -47,6 +115,7 @@ export const Feedback = () => {
                 </button>
 
                 <button
+                    ref={btnBad}
                     onClick={() => increment("bad")}
                     type="button"
                 >
